c5b568d953b3ae74882341f9d33b1611e8bf7b8e
ollielynas <lynasollie@gmail.com>
2022-02-07 18:54:09 +1300

    added frame+buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [matrix, setMatrix] = useState([[" "," "," "],[" "," "," "],[" "," "," "]]);
+
+  const button = (y: number, x: number) => {
+    console.log(y, x);
+    if (matrix[y][x] === " ") {
+      let array: string[][] = matrix;
+      array[y][x] = "X";
+      setMatrix(array)
+      console.log(matrix);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+      className="App-Body"
+      >
+      <div className="App-row">
+        <button className="App-button" onClick={()=>{button(0,0)}} >{matrix[0][0]}</button>
+        <button className="App-button" onClick={()=>{button(0,1)}} >{matrix[0][1]}</button>
+        <button className="App-button" onClick={()=>{button(0,2)}} >{matrix[0][2]}</button>
+      </div>
+      <div className="App-row">
+        <button className="App-button" onClick={()=>{button(1,0)}} >{matrix[1][0]}</button>
+        <button className="App-button" onClick={()=>{button(1,1)}} >{matrix[1][1]}</button>
+        <button className="App-button" onClick={()=>{button(1,2)}} >{matrix[1][2]}</button>
+      </div>
+      <div className="App-row">
+        <button className="App-button" onClick={()=>{button(2,0)}} >{matrix[2][0]}</button>
+        <button className="App-button" onClick={()=>{button(2,1)}} >{matrix[2][1]}</button>
+        <button className="App-button" onClick={()=>{button(2,2)}} >{matrix[2][2]}</button>
+      </div>
+      </div>
     </div>
   );
 }
